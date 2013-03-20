e89bf855317bcf11ba7b289203607f9ca26abe18
orjoseph <orjoseph@gmail.com>
2013-03-19 23:40:35 -0300

    Update sudoku.java

--- sudoku.java
+++ sudoku.java
@@ -1,16 +1,28 @@
-  //methods is supposed to print the sudoku board
+import java.util.*;
+public class sudoku {
+	//methods is supposed to print the sudoku board
 	//with 27 numbers appearing in random areas and 
 	//the numbers are random ranging from 1-9
 	public void printboard(int [][]array)
 	{
 		Random r=new Random();
-		int a,b,cycle=27;
+		int a,b,cycle=0;
 		for(a=0;a<9;a++)
 		{
 			for(b=0;b<9;b++)
 			{
 				//array[a][b]=r.nextInt(9);
-				System.out.print(array[a][b] + " ");
+				if(cycle<27)
+				{
+					System.out.print(array[r.nextInt(9)][r.nextInt(9)]=r.nextInt(9));
+					System.out.print(" ");
+				}
+				else
+				{
+					System.out.print(array[a][b]=0);
+					System.out.print(" ");
+				}
+				cycle++;
 			}
 				System.out.println("\n");
 		}
